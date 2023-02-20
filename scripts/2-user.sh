485f3bf306152215dfc86ba8f565b481b6e87a1f
philipp <philipp@core.com>
2023-02-20 17:52:25 +0100

    fixed wrong filepath and missing makepkg flag

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -7,12 +7,12 @@ echo "##############"
 # install yay
 echo "Installing yay..."
 git clone https://aur.archlinux.org/yay.git /tmp/yay
-cd /tmp/yay && makepkg -si
+cd /tmp/yay && makepkg -si --noconfirm
 rm -rf /tmp/yay
 
 # install packages
 echo "Installing packages..."
-cat /root/archscii-avenger/packages.txt | yay -S --noconfirm -
+cat ~/archscii-avenger/packages.txt | yay -S --noconfirm -
 
 # enable gdm
 sudo systemctl enable gdm.service
